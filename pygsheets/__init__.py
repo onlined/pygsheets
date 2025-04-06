78966195d8c14600c7e56fe439dc5399022c36d3
Ekin Dursun <ekindursun@gmail.com>
2025-04-06 18:40:48 +0300

    Remove custom NullHandler
    Since we don't support Python versions earlier than 2.7, we don't need a custom `NullHandler`, `logging` module already has it.

--- pygsheets/__init__.py
+++ pygsheets/__init__.py
@@ -31,11 +31,5 @@ from pygsheets.exceptions import (PyGsheetsException, AuthenticationError,
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
-try:  # Python 2.7+
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
 
-logging.getLogger(__name__).addHandler(NullHandler())
+logging.getLogger(__name__).addHandler(logging.NullHandler())
